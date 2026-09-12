bcc10f1c6c738dd93021b996447e29e2f2befb2e
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-09-12 11:56:15 +0530

    Add RequestUserType interface and update getUser function to return typed user object

---
+++ types/requestUser.ts
@@ -0,0 +1,7 @@
+export interface RequestUserType {
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  privileges: string[];
+}
--- utils/authentication.ts
+++ utils/authentication.ts
@@ -1,23 +1,25 @@
 import { NextRequest } from "next/server";
 import * as jose from "jose";
+import { RequestUserType } from "@/types/requestUser";
 
-export async function getUser(request : NextRequest) {
+export async function getUser(request : NextRequest) :Promise<RequestUserType | null>{
     const loginToken = request.cookies.get("token")?.value;
 
     const secretText = process.env.JOSE_SECRET;
     const secret = new TextEncoder().encode(secretText);
 
     try{
-        const user = await jose.jwtVerify(loginToken || "", secret);
-        return user.payload;
+        const tokenData = await jose.jwtVerify(loginToken || "", secret);
+        const user: RequestUserType = tokenData.payload as unknown as RequestUserType;
+        return user;
     } catch (error) {
         console.error("Error verifying JWT:", error);
         return null;
     }
 }
 
-export async function isPrivileged(request : NextRequest , privilege : string) {
-    const user = await getUser(request);
+export async function isPrivileged(request : NextRequest , privilege : string): Promise<boolean> {
+    const user : RequestUserType | null = await getUser(request);
     if (!user) {
         return false;
     }
