68785b016e5552d70beed1e94df95341aa2fa983
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-09-24 18:06:10 +0530

    Add pagination to user retrieval and implement profileImage field in User model

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,10 +9,24 @@ export async function GET(request: NextRequest) {
         if(!havePrivilege) {
             return NextResponse.json({message:"You do not have the required privilege to access this resource."}, {status:403});
         }
-    
-        const users = await prisma.user.findMany(
-            {
-                select : {
+
+        const pageNumberIntString = request.nextUrl.searchParams.get("pageNumber")|| "1";
+        const pageSizeIntString = request.nextUrl.searchParams.get("pageSize")|| "10";
+
+        const pageNumber = parseInt(pageNumberIntString)
+        const pageSize =parseInt(pageSizeIntString) 
+
+        const usersCount = await prisma.user.count(); 
+        const totalPages = Math.ceil(usersCount / pageSize);
+
+        if(pageNumber > totalPages){
+            return NextResponse.json({message:"Page number exceeds total pages"}, {status:400});
+        }
+
+        const users = await prisma.user.findMany({
+            skip: (pageNumber - 1) * pageSize,
+            take: pageSize,
+            select : {
                 id: true,
                 email: true,
                 firstName: true,
@@ -22,14 +36,13 @@ export async function GET(request: NextRequest) {
                 lastLogin: true,
                 createdAt: true,
                 privileges: true
-                }
             }
-        );
+        });
 
-    return NextResponse.json({
-        message: "Users retrieved successfully",
-        users : users
-    }) 
+   return NextResponse.json({
+       message: "Users retrieved successfully",
+       users : users
+   }) 
 }
 
 export async function POST(request: NextRequest) {
@@ -72,9 +85,68 @@ export async function PUT(request: NextRequest) {
         return NextResponse.json({message:"Unauthorized"}, {status:401});
     }
 
+    const body = await request.json();
+
     if(requestUser.id == id){
 
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id 
+            }
+        });
+        if(user == null){
+            return NextResponse.json({message:"User not found"}, {status:404});
+        }
+
+        await prisma.user.update({
+            where: {
+                id: id
+            },
+            data: {
+                email: body.email || user.email,
+                firstName: body.firstName || user.firstName,
+                lastName: body.lastName || user.lastName,
+                phone: body.phone || user.phone,
+                profileImage: body.profileImage || user.profileImage
+            }
+
+        });
+
+        return NextResponse.json({message:"User updated successfully"}, {status:200});
+
     }else{
-        
+        const havePrivilege = await isPrivileged(request, "users:edit")
+
+        if(!havePrivilege) {
+            return NextResponse.json({message:"You do not have the required privilege to access this resource."}, {status:403});
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id  || "0000"
+            }
+        });
+        if(user == null){
+            return NextResponse.json({message:"User not found"}, {status:404});
+        }
+
+        await prisma.user.update({
+            where: {
+                id: id|| "0000"
+            },
+            data: {
+                email: body.email || user.email,
+                firstName: body.firstName || user.firstName,
+                lastName: body.lastName || user.lastName,
+                phone: body.phone || user.phone,
+                profileImage: body.profileImage || user.profileImage,
+                role: body.role || user.role,
+                privileges: body.privileges || user.privileges,
+                status: body.status || user.status
+            }
+
+        });
+
+        return NextResponse.json({message:"User updated successfully"}, {status:200});
     }
 }
